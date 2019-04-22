cfcbf1b3e2f21b6573aa590d456a42db36122955
Alexey Petrenko <alx_pet@mail.ru>
2019-04-23 02:12:19 +0300

    Changed API

--- src/main/java/com/selenium/Application.java
+++ src/main/java/com/selenium/Application.java
@@ -2,17 +2,62 @@ package com.selenium;
 
 import com.selenium.config.SeleniumConfig;
 import com.selenium.page.MainPage;
+import com.selenium.page.QuestionsPage;
 import org.openqa.selenium.WebDriver;
 
 public class Application {
 
-    public static void main(String[] args) {
-        WebDriver driver = new SeleniumConfig().getDriver();
+    private WebDriver driver;
+    private MainPage mainPage;
+    public QuestionsPage questionsPage;
+
+    public Application() {
+        driver = new SeleniumConfig().getDriver();
         driver.get("https://www.wrike.com");
-        MainPage page = new MainPage(driver);
-        page.clickGetStartedForFree();
-        page.fillEmail();
-        page.clickCreateAccount().fillProfile();
+        mainPage = new MainPage(driver);
     }
 
+    public void getStartedForFree() {
+        mainPage.clickGetStartedForFree();
+    }
+
+    public void fillEmail() {
+        mainPage.fillEmail();
+    }
+
+    public void createAccountAndMoveToNextPage() {
+        questionsPage = mainPage.clickCreateAccount();
+    }
+
+    public void fillQuestions() {
+        questionsPage.fillProfile();
+    }
+
+    public void resendEmail() {
+        questionsPage.resendEmail();
+    }
+
+    public String getCurrentTitle() {
+        return driver.getTitle();
+    }
+
+    public boolean checkQuestionsFormSubmission() {
+        return !questionsPage.isSubmitResultsButtonVisible();
+    }
+
+    public boolean checkResendingEmail() {
+        return !questionsPage.isResendEmailButtonVisible();
+    }
+
+    public void sleepDriver() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeBrowser() {
+        driver.close();
+    }
 }
